0bdecdc803d595ed8f370305728cfd122710732a
WilcoApp <hello@wilco.gg>
2023-05-23 14:13:27 +0000

    Creating a Harry Potter Broom Order Form

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,45 +1,14 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
-import { connect } from "react-redux";
-import { APPLY_TITLE_FILTER, SET_PAGE } from "../../constants/actionTypes";
-import agent from "../../agent";
 
-const mapStateToProps = (state) => ({
-  ...state.home,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  onSearchTitle: (title, pager, payload) =>
-    dispatch({ type: APPLY_TITLE_FILTER, title, pager, payload }),
-  onGetAll: (page, payload) => dispatch({ type: SET_PAGE, page, payload }),
-});
-
-const Banner = (props) => {
-  const onHandleChange = (searchTerm) => {
-    if (searchTerm.length > 3) {
-      console.log(searchTerm);
-      props.onSearchTitle(
-        searchTerm,
-        (page) => agent.Items.byTitle(searchTerm, page),
-        agent.Items.byTitle(searchTerm)
-      );
-    }
-    if (searchTerm.length === 0) {
-      props.onGetAll(0, agent.Items.all(0));
-    }
-  };
-
+const Banner = () => {
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get</span>
-          <input
-            id="search-box"
-            type="search"
-            onChange={(e) => onHandleChange(e.currentTarget.value)}
-          ></input>
+          <span>A place to </span>
+          <span id="get-part">get</span>
           <span> the cool stuff.</span>
         </div>
       </div>
@@ -47,4 +16,4 @@ const Banner = (props) => {
   );
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Banner);
+export default Banner;
